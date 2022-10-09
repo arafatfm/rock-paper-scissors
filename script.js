1798c03dd68d69c83d5b9ba60be2c2971e00ae7d
arafatfm <mdarafatfahim@gmail.com>
2022-10-10 00:26:17 +0600

    Add playRound() and winCase() function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random()*3)
+    const randomNumber = Math.floor(Math.random()*3)
     switch (randomNumber) {
         case 0:
             return "Rock"
@@ -9,4 +9,35 @@ function getComputerChoice() {
             return "Scissors"
     }
 }
-console.log(getComputerChoice())
+function playRound(playerSelection = "pS", computerSelection = "cS") {
+    let pS = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+    playerSelection = pS
+
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie")
+        return
+    }
+
+    if (playerSelection === winCase(computerSelection)) {
+        console.log("You Win! "+playerSelection+" beats "+computerSelection)
+    } else {
+        console.log("You Lose! "+computerSelection+" beats "+playerSelection)
+    }
+}
+
+function winCase(computerSelection) {
+    switch (computerSelection) {
+        case "Rock":
+            return "Paper"
+        case "Paper":
+            return "Scissors"
+        default:
+            return "Rock"
+    }
+}
+
+
+const playerSelection = prompt("Enter Choice: ")
+const computerSelection = getComputerChoice()
+
+playRound(playerSelection, computerSelection)
